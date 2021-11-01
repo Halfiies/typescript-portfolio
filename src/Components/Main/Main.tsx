98378a76d538e8849266fae4fb00bbb6e034ebc7
Morris Willis <89452634+Halfiies@users.noreply.github.com>
2021-11-01 09:29:30 +0000

    adding tech stack

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -4,11 +4,12 @@ import { Projects } from "../Projects/Projects";
 import { projectArr } from "../../Data/Projects";
 import { ProfileCard } from "../ProfileCard/ProfileCard";
 import { selfProfile } from "../../Data/selfProfile";
-
+import { Techstack } from "../Techstack/Techstack";
 export const Main = () => {
   return (
     <div className="main">
       <ProfileCard selfProfile={selfProfile} />
+      <Techstack />
       <Projects projectArr={projectArr} />
     </div>
   );
---
+++ src/Components/Techstack/Techstack.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Techstack.scss";
+import react_icon from "../../assets/images/react_icon.png";
+import css_icon from "../../assets/images/css.png";
+import html5_icon from "../../assets/images/html5.png";
+import javascript_icon from "../../assets/images/javascript.jpg";
+
+export const Techstack = () => {
+  return (
+    <div className="techStack">
+      <div className="techStack_Content">
+        <img className="techStack__Images" src={react_icon} alt="react_icon" />
+        <img className="techStack__Images" src={css_icon} alt="css_icon" />
+        <img className="techStack__Images" src={html5_icon} alt="html5_icon" />
+        <img
+          className="techStack__Images"
+          src={javascript_icon}
+          alt="javascript_icon"
+        />
+      </div>
+    </div>
+  );
+};
